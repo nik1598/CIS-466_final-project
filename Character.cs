509f9ca52ff4d79806590e6ca32aff7c447ffaf3
Jhansi Kesireddy <37188712+nik1598@users.noreply.github.com>
2019-11-25 20:43:04 -0600

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HeroMonsterBattle_FinalProject
+{
+    public class Character 
+    {
+        // adding properties
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public Character(string name, int health, int damageMaximum, bool attackBonus)
+        {
+            Name = name;
+            Health = health;
+            DamageMaximum = damageMaximum;
+            AttackBonus = attackBonus;
+
+        }
+
+        public int Attack()
+        {
+            var Rand = new Random(this.DamageMaximum); //randomly determine the amount of damange this Character object inflicted
+            return Rand.Next();
+        }
+
+        public void Defend(int damage) //deducts the damage from this Character's health
+        {
+            this.Health -= damage;
+        }
+    }
+}
