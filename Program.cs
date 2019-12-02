ea83363640564879356d30ef93c4707bd8045ea9
c0nbad <55635937+c0nbad@users.noreply.github.com>
2019-12-01 22:07:48 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,20 @@ namespace HeroMonsterBattle_FinalProject
     {
         static void Main(string[] args)
         {
+            
+            //Setting properties for our hero
+            Character hero = new Character();
+            hero.Name = "Hero";
+            hero.Health = 100;
+            hero.DamageMaximum = 20;
+            hero.AttackBonus = false;
+ 
+            //Setting properties for our hero
+            Character monster = new Character();
+            monster.Name = "Monster";
+            monster.Health = 100;
+            monster.DamageMaximum = 18;
+            monster.AttackBonus = false;
         }
     }
 }
