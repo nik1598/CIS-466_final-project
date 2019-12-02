8d4500ff5729ebcde64be4012bbcb4ebd865bd5f
c0nbad <55635937+c0nbad@users.noreply.github.com>
2019-12-01 22:32:34 -0600

    Update Character.cs

--- Character.cs
+++ Character.cs
@@ -1,14 +1,22 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HeroMonsterBattle_FinalProject
 {
-    public class Character 
+    class Character
     {
         // adding properties
         public string Name { get; set; }
         public int Health { get; set; }
         public int DamageMaximum { get; set; }
         public bool AttackBonus { get; set; }
+
+        public Character() //Default constructer
+        {
+        } 
 
         public Character(string name, int health, int damageMaximum, bool attackBonus)
         {
@@ -19,10 +27,13 @@ namespace HeroMonsterBattle_FinalProject
 
         }
 
-        public int Attack()
+       
+
+        public int Attack(Dice dice) // Sets Sides property equal to maximun damage, returns a random int between 0 and DamageMaximum
         {
-            var Rand = new Random(this.DamageMaximum); //randomly determine the amount of damange this Character object inflicted
-            return Rand.Next();
+            dice.Sides = DamageMaximum;     
+            int damageAmount = dice.Roll(); 
+            return damageAmount;
         }
 
         public void Defend(int damage) //deducts the damage from this Character's health
